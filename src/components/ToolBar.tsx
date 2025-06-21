e54b633909df2f8a5eb0a0dd20062eec32fd8bd2
med-hamady <msidiyahyahamady@gmail.com>
2025-06-21 13:16:32 +0200

    Fix Nameh

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -13,7 +13,7 @@ const ToolBar = () => {
         left="50%"
         textAlign={"center"}
       >
-        Kareem Hamouda - Full Stack Developer
+        Mohamed SidiYahya Hamady - Network Engineer || Full Stack Developer
       </Text>
       <ToolBarRight />
     </HStack>
